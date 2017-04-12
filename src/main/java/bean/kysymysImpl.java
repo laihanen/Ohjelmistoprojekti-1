4a8276d0f80e4c8d2da442da431c009a84f2fca6
laihanen <juho.laihanen@gmail.com>
2017-04-12 16:28:38 +0300

    validation

--- src/main/java/bean/kysymysImpl.java
+++ src/main/java/bean/kysymysImpl.java
@@ -1,5 +1,8 @@
 package bean;
 
+
+import javax.validation.constraints.Size;
+
 /**
  * Created by bferr on 12.4.2017.
  */
@@ -7,5 +10,42 @@ public class kysymysImpl {
 
     private int id;
 
+    @Size(min = 1, max = 255)
+    private String kysymysNimi;
     
+    @Size(min = 1, max = 255)
+    private String kysymysTeksti;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getKysymysNimi() {
+        return kysymysNimi;
+    }
+
+    public void setKysymysNimi(String kysymysNimi) {
+        this.kysymysNimi = kysymysNimi;
+    }
+
+    public String getKysymysTeksti() {
+        return kysymysTeksti;
+    }
+
+    public void setKysymysTeksti(String kysymysTeksti) {
+        this.kysymysTeksti = kysymysTeksti;
+    }
+
+    @Override
+    public String toString() {
+        return "kysymysImpl{" +
+                "id=" + id +
+                ", kysymysNimi='" + kysymysNimi + '\'' +
+                ", kysymysTeksti='" + kysymysTeksti + '\'' +
+                '}';
+    }
 }
--- src/main/java/kysely.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Iiro on 12.4.2017.
- */
-public class kysely {
-}
