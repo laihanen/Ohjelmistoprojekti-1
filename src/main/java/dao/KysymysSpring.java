e103cc87b8f0eb0d28baa242e693c30f88ac38e3
laihanen <juho.laihanen@gmail.com>
2017-04-19 11:55:14 +0300

    jaa

--- src/main/java/dao/KysymysSpring.java
+++ src/main/java/dao/KysymysSpring.java
@@ -1,9 +1,20 @@
 package dao;
 
+import bean.Kysymys;
+
 /**
  * Created by bferr on 19.4.2017.
  */
 public class KysymysSpring {
 
-    
+
+
+    public void lisaakysymys(Kysymys t){
+
+        final String sql = "insert into kysymys(otsikko, kysymysteksti) values(?,?)";
+
+        final String otsikko = t.getOtsikko();
+        final String teksti = t.getKysymysteksti();
+
+    }
 }
