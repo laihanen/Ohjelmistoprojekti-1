40afe421a9ac91e45e06ab5f76fd98821b98e3fa
iikeli <iiro.alhonen@gmail.com>
2017-04-19 12:12:32 +0300

    Dao update

--- src/main/java/dao/KysymysDAO.java
+++ src/main/java/dao/KysymysDAO.java
@@ -1,8 +1,15 @@
 package dao;
 
+import java.util.List;
+import bean.Kysymys;
 /**
  * Created by bferr on 19.4.2017.
  */
 public interface KysymysDAO {
 
+    public abstract void tallenna(Kysymys kysymys);
+
+    public abstract Kysymys etsi(int id);
+
+    public abstract List<Kysymykset> haeKaikki();
 }
