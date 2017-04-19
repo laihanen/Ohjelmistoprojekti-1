4e3dab7141d93f59290cc7eb7a388dd6450bd4b3
laihanen <juho.laihanen@gmail.com>
2017-04-19 15:29:48 +0300

    admin

---
+++ src/main/java/control/AdminController.java
@@ -0,0 +1,19 @@
+package control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by bferr on 19.4.2017.
+ */
+@Controller
+@RequestMapping(value="/secure/admin")
+public class AdminController {
+    
+    @RequestMapping(value = "/tools", method = RequestMethod.GET)
+    public String paasivu(Model model) {
+        return "secure/admin/tools";
+    }
+}
