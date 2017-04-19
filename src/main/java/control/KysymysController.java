2216b75d08060d8cd1e010b091780f74276797e6
iikeli <iiro.alhonen@gmail.com>
2017-04-19 14:59:16 +0300

    controller

--- src/main/java/control/KysymysController.java
+++ src/main/java/control/KysymysController.java
@@ -39,8 +39,11 @@ public class KysymysController {
     }
 
     // Form tiedon vastaanotto
-    @
-
+    @RequestMapping (value="id", method=requestMethod.GET)
+        public String create(@ModelAttribute(value="kysymys") KysymysImpl kysymys){
+        dao.tallenna(kysymys);
+        return "redirect:/kysymys/" + kysymys.getId();
+    }
 
     // Tietojen näyttö
 }
