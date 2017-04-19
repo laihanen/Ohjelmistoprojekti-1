4ed6d3b44172370b2b4852e7ec994ae79635de49
iikeli <iiro.alhonen@gmail.com>
2017-04-19 15:28:18 +0300

    SecureController

--- src/main/java/control/SecureController.java
+++ src/main/java/control/SecureController.java
@@ -1,7 +1,26 @@
 package control;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * Created by Iiro on 19.4.2017.
  */
+
+@Controller
+@RequestMapping(value = "/secure")
 public class SecureController {
+
+    @RequestMapping(value = "/main", method = RequestMethod.GET)
+    public String paasivu(Model model){
+        return "secure/main";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String lisaasivu(Model model){
+        return "secure/add";
+    }
+
 }
