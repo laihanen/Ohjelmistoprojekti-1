2e090f8845404f4dde12f6ba63fc078574637184
laihanen <juho.laihanen@gmail.com>
2017-04-20 12:19:56 +0300

    otsikkoInterface

--- src/main/java/bean/Otsikko.java
+++ src/main/java/bean/Otsikko.java
@@ -4,4 +4,13 @@ package bean;
  * Created by bferr on 20.4.2017.
  */
 public interface Otsikko {
+
+    public abstract int getId();
+
+    public abstract void setId(int id);
+
+    public abstract String getOtsikko();
+
+    public abstract void setOtsikko(String otsikko);
+
 }
