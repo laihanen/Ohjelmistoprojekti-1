04d540d9d9bbec7f20fbe477973577fc164d2987
laihanen <juho.laihanen@gmail.com>
2017-04-20 12:10:54 +0300

    otsikkoimpl

--- src/main/java/bean/OtsikkoImpl.java
+++ src/main/java/bean/OtsikkoImpl.java
@@ -1,7 +1,38 @@
 package bean;
 
+import javax.validation.constraints.Size;
+
 /**
  * Created by bferr on 20.4.2017.
  */
 public class OtsikkoImpl {
+
+    private int id;
+
+    @Size(min = 1, max = 255)
+    private String otsikko;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getOtsikko() {
+        return otsikko;
+    }
+
+    public void setOtsikko(String otsikko) {
+        this.otsikko = otsikko;
+    }
+
+    @Override
+    public String toString() {
+        return "OtsikkoImpl{" +
+                "id=" + id +
+                ", otsikko='" + otsikko + '\'' +
+                '}';
+    }
 }
