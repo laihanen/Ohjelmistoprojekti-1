2f933e2d3f871d3ec282818c4e87fb850ba351e7
laihanen <juho.laihanen@gmail.com>
2017-04-20 13:35:07 +0300

    kysely ja kyselyImpl

---
+++ src/main/java/bean/Kysely.java
@@ -0,0 +1,15 @@
+package bean;
+
+/**
+ * Created by bferr on 20.4.2017.
+ */
+public interface Kysely {
+
+    public abstract int getId();
+
+    public abstract void setId(int id);
+
+    public abstract String getNimi();
+
+    public abstract void setNimi(String nimi);
+}
