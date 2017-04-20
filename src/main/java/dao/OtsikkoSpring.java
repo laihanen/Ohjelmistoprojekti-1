bb908aae27ee212d845d397cf32935ad0bc314c4
iikeli <iiro.alhonen@gmail.com>
2017-04-20 12:20:50 +0300

    OtsikkoSpring

--- src/main/java/dao/OtsikkoSpring.java
+++ src/main/java/dao/OtsikkoSpring.java
@@ -1,7 +1,31 @@
 package dao;
 
+import bean.Otsikko;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import javax.inject.Inject;
+
 /**
  * Created by bferr on 20.4.2017.
  */
-public class OtsikkoSpring {
+@Repository
+public class OtsikkoSpring implements OtsikkoDAO {
+
+    @Inject
+    public JdbcTemplate jdbcTemplate;
+
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) { this.jdbcTemplate = jdbcTemplate; }
+
+    public void lisaaOtsikko(Otsikko t){
+
+        final String sql = "insert into otsikko(otsikko) values (?)";
+        final String otsikko = t.getOtsikko();
+
+        KeyHolder idHolder = new GeneratedKeyHolder();
+    }
+
+
 }
