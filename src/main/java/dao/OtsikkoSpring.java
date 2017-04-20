cd0006262a93fe9d6ffa8b4a98c512ee33df07e8
iikeli <iiro.alhonen@gmail.com>
2017-04-20 12:36:11 +0300

    OtsikkoSpring

--- src/main/java/dao/OtsikkoSpring.java
+++ src/main/java/dao/OtsikkoSpring.java
@@ -2,11 +2,17 @@ package dao;
 
 import bean.Otsikko;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import javax.inject.Inject;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
 
 /**
  * Created by bferr on 20.4.2017.
@@ -25,8 +31,30 @@ public class OtsikkoSpring implements OtsikkoDAO {
         final String otsikko = t.getOtsikko();
 
         KeyHolder idHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(new PreparedStatementCreator() {
+            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                PreparedStatement pre = con.prepareStatement(sql, new String[]{"id"});
+                pre.setString(1, otsikko);
+                return pre;
+            }
+        }, idHolder);
+        t.setId(idHolder.getKey().intValue());
     }
 
-    // Juho alapuolelle
+    public void poista(int id){
 
+        String sql = "delete from otsikko where id = ?";
+        jdbcTemplate.update(sql, id);
+        System.out.println("Deleted record with ID = " + id);
+        return;
+    }
+
+    public List<Otsikko> haeKaikki(){
+
+        String sql = "select id, otsikko from otsikko";
+        RowMapper<Otsikko> mapper = new otsikkoRowMapper();
+        List<Otsikko> otsikot = jdbcTemplate.query(sql, mapper);
+        return otsikot;
+    }
 }
