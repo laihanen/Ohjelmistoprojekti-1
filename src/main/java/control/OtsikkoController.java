bdf49abf849273d7dc58deda11fe33df3fd9db68
iikeli <iiro.alhonen@gmail.com>
2017-04-20 13:13:32 +0300

    OtsikkoController

--- src/main/java/control/OtsikkoController.java
+++ src/main/java/control/OtsikkoController.java
@@ -5,10 +5,13 @@ import bean.OtsikkoImpl;
 import dao.OtsikkoDAO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.inject.Inject;
+import java.util.List;
 
 /**
  * Created by bferr on 20.4.2017.
@@ -33,7 +36,16 @@ public class OtsikkoController {
     }
 
     // Vastaanotto
-
+    @RequestMapping (value ="id", method = RequestMethod.GET)
+    public String create(@ModelAttribute(value = "otsikko") OtsikkoImpl otsikko){
+        dao.lisaaOtsikko(otsikko);
+        return "redirect:/otsikko/" + otsikko.getId();
+    }
     // Näyttö
-
+    @RequestMapping (value = "id", method = RequestMethod.GET)
+    public String getView(@PathVariable Integer id, Model model){
+        List<Otsikko> otsikko = dao.haeKaikki();
+        model.addAttribute("otsikko", otsikko);
+        return "secure/view";
+    }
 }
