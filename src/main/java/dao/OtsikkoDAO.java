7e571720baffb32939cbf51c7dfcf354df7982ba
iikeli <iiro.alhonen@gmail.com>
2017-04-20 12:08:02 +0300

    OtsikkoDAO

--- src/main/java/dao/OtsikkoDAO.java
+++ src/main/java/dao/OtsikkoDAO.java
@@ -1,7 +1,17 @@
 package dao;
 
+import bean.Otsikko;
+
+import java.util.List;
+
 /**
  * Created by bferr on 20.4.2017.
  */
 public interface OtsikkoDAO {
+
+    public abstract void lisaaOtsikko(Otsikko otsikko);
+
+    public abstract void poista(int id);
+
+    public abstract List<Otsikko> haeKaikki();
 }
