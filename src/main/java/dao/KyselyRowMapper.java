6055b71500ee07621998168fee2d5eef489b936b
iikeli <iiro.alhonen@gmail.com>
2017-04-21 11:12:02 +0300

    KyselyRowMapper

--- src/main/java/dao/KyselyRowMapper.java
+++ src/main/java/dao/KyselyRowMapper.java
@@ -1,7 +1,23 @@
 package dao;
 
+
+import bean.Kysely;
+import bean.KyselyImpl;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * Created by Iiro on 20.4.2017.
  */
-public class KyselyRowMapper {
+public class KyselyRowMapper implements RowMapper<Kysely> {
+
+    public Kysely mapRow(ResultSet rs, int rowNum) throws SQLException{
+        Kysely m = new KyselyImpl();
+        m.setId(rs.getInt("id"));
+        m.setNimi(rs.getString("nimi"));
+
+        return m;
+    }
 }
