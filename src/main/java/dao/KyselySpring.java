08f2bb75a16ca77c136d0a67389dd85d3b4b1d33
laihanen <juho.laihanen@gmail.com>
2017-04-21 11:11:34 +0300

    Spring

--- src/main/java/dao/KyselySpring.java
+++ src/main/java/dao/KyselySpring.java
@@ -1,7 +1,53 @@
 package dao;
 
+import bean.Kysely;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import javax.inject.Inject;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  * Created by Iiro on 20.4.2017.
  */
-public class KyselySpring {
+@Repository
+public class KyselySpring implements KyselyDAO {
+
+    @Inject
+    public JdbcTemplate jdbcTemplate;
+
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void lisaaKysely(Kysely k){
+
+        final String sql = "insert into kysely(nimi) values(?)";
+        final String nimi = k.getNimi();
+
+        KeyHolder idHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(new PreparedStatementCreator() {
+            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                PreparedStatement pre = con.prepareStatement(sql, new String[]{"id"});
+                pre.setString(1, nimi);
+                return pre;
+            }
+        }, idHolder);
+        k.setId(idHolder.getKey().intValue());
+    }
+
+    public void poista(int id){
+
+        String sql = "delete from kysely where id = ?";
+        jdbcTemplate.update(sql, id);
+        System.out.println("Deleted Record with ID = " + id );
+        return;
+    }
+
 }
